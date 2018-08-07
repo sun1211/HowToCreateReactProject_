594ccbc2ff7b48a513cf1efd7a3193ebe48e1167
sun1211 <ngoctam121191@yahoo.com>
2018-08-07 16:41:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App.jxs";
+
+// ReactDOM.render(<App />,document.getElementById('MyApp'));
+ReactDOM.render(
+    <div>sdfsdfsdf</div>,
+    document.getElementById('MyApp')
+  );
