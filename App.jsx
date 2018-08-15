48dba9135b142bc7599fb8186f31ae0f32cf4724
sun1211 <ngoctam121191@yahoo.com>
2018-08-15 21:06:55 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-class App extends React.Component {
-   render() {
-      return (
-         <h1>
-            Hello World!!! Test Button 
-            <button type="button" class="btn btn-primary">Primary</button>
-         </h1>
-      );
-   }
+// We can inject some CSS into the DOM.
+const styles = {
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+};
+
+class App extends React.Component{
+    render(){
+        return(
+            <Button className={this.props.classes.button}>
+            {this.props.children ? this.props.children : 'class names'}
+          </Button>
+        )
+    }
 }
-export default App;
+
+
+App.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
