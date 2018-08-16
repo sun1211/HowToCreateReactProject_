6a470f85407c3878adc28812aadaf369d24300d7
sun1211 <ngoctam121191@yahoo.com>
2018-08-16 10:47:05 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 
-// We can inject some CSS into the DOM.
-const styles = {
-  button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  },
-};
+const styles = theme =>(
+  {
+    '@global': {
+      body: {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+  
+    layout: {
+      width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+        width: 900,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+  }
+)
 
-class App extends React.Component{
-    render(){
-        return(
-            <Button className={this.props.classes.button}>
-            {this.props.children ? this.props.children : 'class names'}
-          </Button>
-        )
-    }
+// class App extends React.Component{
+//   constructor(props) {
+//     super(props)
+//   };
+  
+//     render(){
+//       const  {classes}  = this.props;
+//         return(
+//           <div className={classes.layout}> Test With Class </div>
+//         )
+//     }
+// }
+
+//test with function
+function App(props){
+  const{ classes } = props;
+  return(
+    <div className={classes.layout}> Test With Function </div>
+  )
 }
-
-
-App.propTypes = {
-  children: PropTypes.node,
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(App);
